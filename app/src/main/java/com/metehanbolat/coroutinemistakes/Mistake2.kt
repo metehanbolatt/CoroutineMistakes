a23c5fd1c42fc05b29098a8390d0e33ea8dc8fda
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-02-13 21:00:27 +0300

    Solution-2 -> use isActive or ensureActive()

--- app/src/main/java/com/metehanbolat/coroutinemistakes/Mistake2.kt
+++ app/src/main/java/com/metehanbolat/coroutinemistakes/Mistake2.kt
@@ -1,16 +1,15 @@
 package com.metehanbolat.coroutinemistakes
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import kotlin.random.Random
 
 suspend fun doSomething() {
     val job = CoroutineScope(Dispatchers.Default).launch {
         var random = Random.nextInt(100_000)
-        while (random != 50000) {
+        while (random != 50000 && isActive) {
             random = Random.nextInt(100_000)
+            // or we can use "ensureActive() instead of "isActive".
+            // ensureActive()
         }
     }
     delay(500L)
